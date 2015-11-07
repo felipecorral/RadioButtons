4609b1f9274a434f7fd8a49f55b4c333ab9a3033
felipecorral <felipecorral@gmail.com>
2015-11-08 00:30:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a14felipecm/radiobuttons/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.a14felipecm.radiobuttons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private boolean isChecking = true;
+    private int mCheckedId = R.id.rbuno;
+
+    RadioGroup rg2;
+    RadioGroup rg3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rg2 = (RadioGroup) findViewById(R.id.rgbotones2);
+        rg3 = (RadioGroup) findViewById(R.id.rgbotones3);
+
+
+
+
+
+        rg2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId != -1 && isChecking) {
+                    isChecking = false;
+                    rg3.clearCheck();
+                    mCheckedId = checkedId;
+                }
+                isChecking = true;
+            }
+        });
+
+        rg3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId != -1 && isChecking) {
+                    isChecking = false;
+                    rg2.clearCheck();
+                    mCheckedId = checkedId;
+                }
+                isChecking = true;
+            }
+        });
+
+
+
+
+    }
+
+
+
+
+}
